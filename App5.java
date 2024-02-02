39bc365c09e6870889ea1caf4a9bf20d9a1eee1b
PriyalS6849 <priyalsaxena6849@gmail.com>
2024-02-02 10:12:49 +0530

    ye89

--- App5.java
+++ App5.java
@@ -1,9 +1,8 @@
-public class App5 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+public class App5{
+    public static void main(String[] args){
+        System.out.println("Java App Pulled & Build by Jenkins");
+        System.out.println("Hello Java and Jenkins");
+        System.out.println("Hello World");
         
-         
     }
 }
